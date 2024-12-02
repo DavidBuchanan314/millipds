f68b9bdd784bf43327ef073673561fdc103ef02f
David Buchanan <d@vidbuchanan.co.uk>
2024-12-02 09:04:04 +0000

    more unfinished wip oauth stuff

--- src/millipds/oauth.py
+++ src/millipds/oauth.py
@@ -2,6 +2,7 @@ import logging
 
 import jwt
 import cbrrr
+import json
 
 from aiohttp import web
 
@@ -64,17 +65,103 @@ async def oauth_authorization_server(request: web.Request):
 
 @routes.get("/oauth/authorize")
 async def oauth_authorize(request: web.Request):
+	# TODO: extract request_uri
 	return web.Response(
-		text="<h1>TODO: login</h1>",
-		content_type="text/html"
+		text="""\
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Authorize</title>
+		<style>
+			/*html, body {
+				height: 100%
+			}*/
+			body {
+				background-color: #333;
+				color: #fff;
+				font-family: system-ui, sans-serif;
+				font-size: 16pt;
+				margin: 2em 0;
+			}
+
+			.panel {
+				background-color: #222;
+				max-width: 420px;
+				margin: auto;
+				padding: 1.5em;
+			}
+
+			h1 {
+				margin: 0;
+				border-bottom: 0.1em solid #ff0048;
+				line-height: 0.9;
+			}
+
+			form {
+				margin-top: 1.5em;
+			}
+
+			input {
+				width: 100%;
+				box-sizing: border-box;
+				padding: 0.5em 0.6em;
+				font-size: 16pt;
+				margin-bottom: 1em;
+				margin-top: 0.5em;
+				background-color: #1a1a1a;
+				color: #fff;
+				/*border: 0.1px solid #888;*/
+				border-style: none;
+				/*border-radius: 4px;*/
+			}
+
+			input[type="submit"] {
+				/*margin-top: 1em;*/
+				background-color: #ff0048;
+				font-weight: bold;
+				box-shadow: 2px 2px #000;
+				margin-bottom: 0;
+				/*border: 0.1px solid #fff;*/
+			}
+
+			input[type="submit"]:hover {
+				background-color: #e10042;
+			}
+
+			input[type="submit"]:active {
+				background-color: #c00038;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="panel">
+			<h1>millipds</h1>
+			<h3>put yer creds in the box.</h2>
+			<form action="/oauth/login" method="POST">
+				<div class="box">
+					<label>handle: <input type="text" name="handle" value="todo.invalid" placeholder="bob.example.org"></label>
+					<label>password: <input type="password" name="password" placeholder="password"></label>
+				</div>
+				<input type="submit" value="sign in">
+			</form>
+		</div>
+	</body>
+</html>""",
+		content_type="text/html",
+		headers={
+			"Content-Security-Policy": "default-src 'none'; img-src 'self'; style-src 'unsafe-inline'"
+		}
 	)
 
+DPOP_NONCE = "placeholder_nonce_value" # this needs to get rotated! (does it matter that it's global?)
 
 def dpop_protected(handler):
-	def dpop_handler(request: web.Request):
+	async def dpop_handler(request: web.Request):
 		dpop = request.headers.get("dpop")
 		if dpop is None:
-			raise web.HTTPUnauthorized(
+			raise web.HTTPBadRequest(
 				text="missing dpop"
 			)
 
@@ -82,7 +169,7 @@ def dpop_protected(handler):
 		unverified = jwt.api_jwt.decode_complete(dpop, options={"verify_signature": False})
 		jwk_data = unverified["header"]["jwk"]
 		jwk = jwt.PyJWK.from_dict(jwk_data)
-		decoded = jwt.decode(dpop, key=jwk) # actual signature verification happens here
+		decoded: dict = jwt.decode(dpop, key=jwk) # actual signature verification happens here
 
 		logger.info(decoded)
 		logger.info(request.url)
@@ -90,36 +177,52 @@ def dpop_protected(handler):
 		# TODO: verify iat?, iss?
 
 		if request.method != decoded["htm"]:
-			raise web.HTTPUnauthorized(
+			return web.HTTPBadRequest(
 				text="dpop: bad htm"
 			)
 
 		if str(request.url) != decoded["htu"]:
 			logger.info(f"{request.url!r} != {decoded['htu']!r}")
-			raise web.HTTPUnauthorized(
+			return web.HTTPBadRequest(
 				text="dpop: bad htu (if your application is reverse-proxied, make sure the Host header is getting set properly)"
+			)
+		
+		if decoded.get("nonce") != DPOP_NONCE:
+			return web.HTTPBadRequest(
+				body=json.dumps({
+					"error": "use_dpop_nonce",
+					"error_description": "Authorization server requires nonce in DPoP proof"
+				}),
+				headers={
+					"DPoP-Nonce": DPOP_NONCE,
+					"Content-Type": "application/json"
+				} # if we don't put it here, the client will never see it
 			)
 
 		request["dpop_jwk"] = cbrrr.encode_dag_cbor(jwk_data) # for easy comparison in db etc.
 		request["dpop_jti"] = decoded["jti"] # XXX: should replay prevention happen here?
 		request["dpop_iss"] = decoded["iss"]
 
-		return handler(request)
+		res: web.Response = await handler(request)
+		res.headers["DPoP-Nonce"] = DPOP_NONCE # TODO: make sure this always gets set even under error conditions?
+		return res
 
 	return dpop_handler
 
 
 @routes.post("/oauth/par")
 @dpop_protected
-async def oauth_par(request: web.Request):
+async def oauth_pushed_authorization_request(request: web.Request):
 	data = await request.json() # TODO: doesn't rfc9126 say it's posted as form data?
 	logging.info(data)
 
 	assert(data["client_id"] == request["dpop_iss"]) # idk if this is required
 
-	# TODO: rest of owl
+	# we need to store the request somewhere, and associate it with the URI we return
+
 	return web.json_response({
-		"TODO": "TODO"
+		"request_uri": "urn:ietf:params:oauth:request_uri:req-064ed63e9fbf10815fd5f402f4f3e92a", #XXX hardcoded test
+		"expires_in": 299
 	})
 
 
