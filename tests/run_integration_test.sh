faadf4c4e84ded7e6c7292569da43c7ab77bd9e8
David Buchanan <d@vidbuchanan.co.uk>
2024-12-02 12:46:15 +0000

    maybe-working ci test

---
+++ tests/run_integration_test.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -euxo pipefail
+
+rm -rf ./data
+
+millipds init localhost:8123
+millipds account create did:plc:bwxddkvw5c6pkkntbtp2j4lx local.dev.retr0.id --unsafe_password="lol"
+
+millipds run &
+PDS_PID=$!
+
+# wait for server to start up
+until curl -s http://localhost:8123/
+do
+	echo "waiting for service startup"
+	sleep 0.05
+done
+
+python3 tests/integration_test.py
+kill $PDS_PID
